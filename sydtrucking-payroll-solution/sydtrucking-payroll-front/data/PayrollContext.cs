29a72aa20bdbe32435125db6ad8d919698444093
Michael Navarro Ariza <rotpar@gmail.com>
2019-03-17 08:00:19 -0500

    Se agrega clase de contexto, valores de configuración para cadena de conexion y clase de modelo

---
+++ sydtrucking-payroll-solution/sydtrucking-payroll-front/data/PayrollContext.cs
@@ -0,0 +1,17 @@
+﻿namespace sydtrucking_payroll_front.data
+{
+    using MongoDB.Driver;
+    using sydtrucking_payroll_front.Properties;
+
+    public class PayrollContext
+    {
+        private readonly IMongoDatabase _database = null;
+
+        public PayrollContext(Settings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            if (client != null)
+                _database = client.GetDatabase(settings.Database);
+        }
+    }
+}
