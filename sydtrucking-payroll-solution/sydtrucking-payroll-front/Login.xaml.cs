3a7ab4b353d834424349a242fd207dbe1e9b9be3
Michael Navarro Ariza <rotpar@gmail.com>
2019-04-27 23:42:18 -0500

    Se agrega pantalla de login

---
+++ sydtrucking-payroll-solution/sydtrucking-payroll-front/Login.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace sydtrucking_payroll_front
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Lógica de interacción para Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void TruckButton_Click(object sender, RoutedEventArgs e)
+        {
+            view.Trucks truck = new view.Trucks();
+            truck.Show();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (SocialSecurity.Text=="admin" && Password.Password == "admin")
+            {
+                MainWindow main = new MainWindow();
+                main.Show();
+
+                Close();
+            }
+        }
+    }
+}
